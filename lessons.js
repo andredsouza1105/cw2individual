38313d0d4f8d6e820b1632091f8d6004f0f4b8d4
andredsouza1105 <andredsouza1105@gmail.com>
2023-02-13 12:08:30 +0400

    added files from cw1

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let Lessons = [
+    {
+        id: 101,
+        subject:"math",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/math.jpg"
+    },
+    {
+        id: 102,
+        subject:"science",
+        location:"dubai",
+        price:1500,
+        spaces:5,
+        image:"images/science.jpg"
+    },
+    {
+        id: 103,
+        subject:"english",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/english.jpg"
+    },
+    {
+        id: 104,
+        subject:"Social Studies",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/sst.jpg"
+    },
+    {
+        id: 105,
+        subject:"French",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/french.jpg"
+    },
+    {
+        id: 106,
+        subject:"spanish",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/spanish.jpg"
+    },
+    {
+        id: 107,
+        subject:"computer science",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/cs.jpg"
+    },
+    {
+        id: 108,
+        subject:"Geography",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/geo.jpg"
+    },
+    {
+        id: 109,
+        subject:"Culinary",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/culinary.jpg"
+    },
+    {
+        id: 110,
+        subject:"accounting",
+        location:"dubai",
+        price:2000,
+        spaces:5,
+        image:"images/accounting.jpg"
+    },
+]
